55aeab9cf535efed62d9c0c2589188735fd96f32
sachinNishalka <nishalkasachin@gmail.com>
2023-02-16 09:56:49 +0530

    finished project

---
+++ new.cs
@@ -0,0 +1,10 @@
+class new{
+    static void Main(string[] args){
+       string name;
+       Console.WriteLine("Enter your name :");
+       name = Console.ReadLine();
+
+       Console.WriteLine("Welcome " + name);
+       
+    }
+}
